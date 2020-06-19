5ee5d96428e0c3bfc00bf73214038852d8692dc2
Raykhan12 <45717532+Raykhan12@users.noreply.github.com>
2020-06-19 00:49:10 -0400

    dfsd

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'PaginaWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'djangobd',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
+        }
 }
 
 
